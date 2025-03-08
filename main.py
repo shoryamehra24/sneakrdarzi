df81a71b4a89f1b5afe58722d203e5b83bf8de31
Shorya Mehra <66180285+shoryamehra24@users.noreply.github.com>
2025-03-09 03:16:46 +0530

    Update and rename app.py to main.py

--- main.py
+++ main.py
@@ -4,12 +4,16 @@ import json
 app = Flask(__name__)
 
 # Load sneaker data from JSON file
-with open('sneakers.json', 'r') as f:
+with open('sneakers_with_price.json', 'r') as f:
     sneakers_data = json.load(f)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('home.html')  # Serve the main interface
+
+@app.route('/shoe-box')
+def shoe_box():
+    return render_template('shoe-box.html')  # Serve the Shoe Box page
 
 @app.route('/api/sneakers', methods=['GET'])
 def get_sneakers():
